cf31dc47896b6f6e2668ee34e904535db7dbc3b6
nadiashovkovy <nadiashovkovy@gmail.com>
2023-07-20 17:39:43 -0700

    Initial Commit

---
+++ VolunTeen AZ/VolunTeen_AZApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolunTeen_AZApp.swift
+//  VolunTeen AZ
+//
+//  Created by Nadia Shovkovy on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct VolunTeen_AZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VolunTeen AZUITests/VolunTeen_AZUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VolunTeen_AZUITestsLaunchTests.swift
+//  VolunTeen AZUITests
+//
+//  Created by Nadia Shovkovy on 7/20/23.
+//
+
+import XCTest
+
+final class VolunTeen_AZUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
